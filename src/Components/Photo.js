f8e5313a0db8c532564bb7824ed50c7567ef2419
anand-gaurav <gaurav1581@gmail.com>
2018-08-29 11:14:39 +0530

    Second commit

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -5,7 +5,9 @@ class Photo extends Component {
         const post = this.props.post;
         return (
         <figure className="figure">
-            <img src={post.imageLink} alt={post.description}/>
+            <img className="photo" src={post.imageLink} alt={post.description}/>
+            <figcaption><p>{post.description}</p></figcaption>
+            <button>Remove</button>
         </figure>
         );
     }
